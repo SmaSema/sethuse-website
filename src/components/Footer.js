f90b2a8acf4fbb617fd2d6fd1d4954aa2c25d3b1
SmaMasase <smangalenecharles@gmail.com>
2025-06-30 23:32:50 +0200

    Eleventh  commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,48 +1,51 @@
 // src/components/Footer.js
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css';
-import { FaFacebookF, FaInstagram } from 'react-icons/fa';
+import { FaFacebookF, FaInstagram, FaMapMarkerAlt } from 'react-icons/fa';
 import { SiTiktok } from 'react-icons/si';
-
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer-container">
-        {/* Left: Logo or name */}
+        {/* Left: Name and Address */}
         <div className="footer-logo">
           <h2>Sethuse</h2>
           <p>Empowering communities, transforming lives.</p>
+          <div className="footer-address">
+            <FaMapMarkerAlt size={16} style={{ marginRight: '8px' }} />
+            <span>516 Unit 18, Imbali, Pietermaritzburg, 3201</span>
+          </div>
         </div>
 
         {/* Middle: Quick links */}
         <div className="footer-links">
           <h3>Quick Links</h3>
           <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#work">Work</a></li>
-            <li><a href="#events">Events</a></li>
-            <li><a href="#donate">Donate</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">Work</a></li>
+            <li><a href="/work">Programs</a></li>
+            <li><a href="/donate">Donate</a></li>
           </ul>
         </div>
 
         {/* Right: Social media */}
         <div className="footer-social">
-        <h3>Follow Us</h3>
-        <div className="social-icons">
+          <h3>Follow Us</h3>
+          <div className="social-icons">
             <a href="https://www.facebook.com/profile.php?id=100092553245300#" target="_blank" rel="noopener noreferrer">
-            <FaFacebookF size={20} />
+              <FaFacebookF size={20} />
             </a>
             <a href="https://www.tiktok.com/search?q=sethuse%20community%20haven&t=1751316789756" target="_blank" rel="noopener noreferrer">
-            <SiTiktok size={20} />
+              <SiTiktok size={20} />
             </a>
             <a href="https://www.instagram.com/sethuse_community_haven_npo/" target="_blank" rel="noopener noreferrer">
-            <FaInstagram size={20} />
+              <FaInstagram size={20} />
             </a>
+          </div>
         </div>
-        </div>
-
       </div>
 
       <div className="footer-bottom">
