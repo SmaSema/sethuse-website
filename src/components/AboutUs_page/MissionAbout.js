42b84ff6a222b42637eee2614740d199fab3f0ce
SmaMasase <smangalenecharles@gmail.com>
2025-07-01 20:57:17 +0200

    20th  commit

--- src/components/AboutUs_page/MissionAbout.js
+++ src/components/AboutUs_page/MissionAbout.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import '../AboutUs_page/MissionAbout.css';
+import hikeImage from  '/Users/smang/sethuse-website/src/assets/Hike.jpg'
 
 const MissionSection = () => {
   return (
@@ -9,16 +10,16 @@ const MissionSection = () => {
       <div className="mission-text">
         <h2>Our Mission</h2>
         <p>
-            Sethuse Community Haven is on a mission to create a 
-            haven of support and empowerment for those in need. 
-            Through collaborative efforts and community 
-            engagement, we aim to foster positive change, uplift 
-            the marginalized, and build a resilient and inclusive 
-            society. 
+          Sethuse Community Haven is on a mission to create a 
+          haven of support and empowerment for those in need. 
+          Through collaborative efforts and community 
+          engagement, we aim to foster positive change, uplift 
+          the marginalized, and build a resilient and inclusive 
+          society. 
         </p>
       </div>
       <div className="mission-image">
-        <img src="/src/assets/mission.jpg" alt="Community support activity" />
+        <img src={hikeImage} alt="Community support activity" />
       </div>
     </section>
   );
