fe17dc96eee20e65f6bd8ce9832a025741b3f4f9
SmaMasase <smangalenecharles@gmail.com>
2025-07-05 21:18:55 +0200

    36th  commit

--- src/components/OurWork_page/HeroWork.js
+++ src/components/OurWork_page/HeroWork.js
@@ -4,7 +4,7 @@ import '../OurWork_page/HeroWork.css'
 const HeroWork = () => (
   <div className="header-block">
       <h4>What We Do</h4>
-      <p>Sethuse supports vibrant communities through education, care, and connection.</p>
+      <p>At Sethuse Community Haven, we believe that real change begins with intentional action. Our objectives reflect the heart of our organization: to uplift, educate, and empower. Whether we’re mentoring the youth, supporting the elderly, or standing for inclusivity and equality, each goal represents our commitment to building a resilient, united community. Below are the guiding pillars that shape everything we do.</p>
     </div>
 );
 
