dcbaeb0e34a9f019b9dc2c026b5be6105fa85af0
SmaMasase <smangalenecharles@gmail.com>
2025-07-05 21:09:51 +0200

    34th  commit

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,12 +1,10 @@
 // src/pages/Work.js
 import React from 'react';
 import HeroWork from '../components/OurWork_page/HeroWork';
-import ProgramsWork from '../components/OurWork_page/ProgramsWork'
 const OurWork = () => {
   return (
     <>
       <HeroWork />
-      <ProgramsWork />
     </>
   );
 };
