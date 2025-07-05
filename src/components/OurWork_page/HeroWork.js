8afb695cd694faf1c8f84b613ec05dea1b79fdc7
SmaMasase <smangalenecharles@gmail.com>
2025-07-05 21:11:45 +0200

    35th  commit

--- src/components/OurWork_page/HeroWork.js
+++ src/components/OurWork_page/HeroWork.js
@@ -5,7 +5,6 @@ const HeroWork = () => (
   <div className="header-block">
       <h4>What We Do</h4>
       <p>Sethuse supports vibrant communities through education, care, and connection.</p>
-      <button className="explore-btn">Explore our programs</button>
     </div>
 );
 
