027446385ac5afdece2dc49c9ff613f92ce5c62b
SmaMasase <smangalenecharles@gmail.com>
2025-11-05 23:22:07 +0200

    Commit 115

--- src/components/AboutUs_page/CTABanner.js
+++ src/components/AboutUs_page/CTABanner.js
@@ -6,7 +6,7 @@
 
 import React from 'react';
 import '../AboutUs_page/CTABanner.css'; // Import corresponding CSS
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 const CTABanner = () => {
   return (
